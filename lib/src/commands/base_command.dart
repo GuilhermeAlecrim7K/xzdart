c34c38579a51ce29c8b321e3b7e1c4dd0808754a
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2022-10-12 16:31:08 -0300

    Adjustments to BaseCommand class

--- lib/src/commands/base_command.dart
+++ lib/src/commands/base_command.dart
@@ -1,16 +1,25 @@
 import 'package:args/command_runner.dart';
 import 'package:xzdart_solutions/xzdart_solutions.dart';
 
-abstract class BaseCommand extends Command{
-
+abstract class BaseCommand extends Command {
   final BaseExercise exercise;
 
-  BaseCommand({required this.exercise}): super();
+  BaseCommand({required this.exercise});
 
   @override
-  String get description => '${exercise.instruction}\n\n'
-    'Description\n${exercise.description}';
+  String get description => '${exercise.title.toUpperCase()}\n\n'
+      'Level: ${exercise.level.name.capitalized()}\n\n'
+      'Task: ${exercise.instruction}\n\n'
+      'Description:\n${exercise.description}';
+
+  @override
+  String get summary =>
+      '${'<${exercise.level.name.capitalized()}>'.padRight(8)} ${exercise.instruction}';
 
   @override
   String get invocation => 'xzdart $name [arguments]';
+}
+
+extension _Capitalizer on String {
+  String capitalized() => this[0].toUpperCase() + substring(1);
 }
